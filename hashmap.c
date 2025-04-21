32189e04b36c8a5f96f8e95d6d0dc351b8c432c1
Your Name <you@example.com>
2025-04-20 20:40:44 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -38,20 +38,11 @@ int is_equal(void* key1, void* key2){
     return 0;
 }
 
-/*Implemente la función void insertMap(HashMap * map, char * key, void * value). Esta función inserta un nuevo dato 
-(key,value) en el mapa y actualiza el índice current a esa posición.
-Recuerde que para insertar un par (clave,valor) debe:
-
+/*Implemente la función void insertMap:
 a - Aplicar la función hash a la clave para obtener la posición donde debería insertar el nuevo par
-
 b - Si la casilla se encuentra ocupada, avance hasta una casilla disponible (*método de resolución de colisiones*). 
 Una casilla disponible es una casilla nula, pero también una que tenga un par inválido (key==NULL).
-
-c - Ingrese el par en la casilla que encontró.
-
-No inserte claves repetidas. 
-Recuerde que el arreglo es **circular**.
-Recuerde actualizar la variable size.*/
+c - Ingrese el par en la casilla que encontró.*/
 
 void insertMap(HashMap * map, char * key, void * value) {
     long pos = hash(key, map->capacity); //a
@@ -77,8 +68,7 @@ void enlarge(HashMap * map) {
 1.- Esta función crea una variable de tipo HashMap, 
 2.- inicializa el arreglo de buckets con casillas nulas, 
 3.- inicializa el resto de variables y retorna el mapa. 
-4.- Inicialice el índice current a -1.
-*/
+4.- Inicialice el índice current a -1.*/
 HashMap * createMap(long capacity) {
     HashMap * map = malloc(sizeof(HashMap));//1
     
@@ -98,9 +88,23 @@ void eraseMap(HashMap * map,  char * key) {
 
 }
 
-Pair * searchMap(HashMap * map,  char * key) {   
+/*3.- Implemente la función Pair * searchMap(HashMap * map,  char * key), la cual retorna el **Pair** asociado a la clave ingresada. 
+Recuerde que para buscar el par debe:
+a - Usar la función hash para obtener la posición donde puede encontrarse el par con la clave
+b - Si la clave no se encuentra avance hasta encontrarla (*método de resolución de colisiones*)
+c - Si llega a una casilla nula, retorne NULL inmediatamente (no siga avanzando, la clave no está)
+Recuerde actualizar el índice current a la posición encontrada.
+Recuerde que el arreglo es **circular**.*/
 
-
+Pair * searchMap(HashMap * map,  char * key) {
+    long pos = hash(key, map->capacity); //a
+    while (map->buckets[pos] != NULL) { //b
+        if (is_equal(map->buckets[pos]->key, key)) {
+            map->current = pos; //actualiza el current
+            return map->buckets[pos];
+        }
+        pos = (pos + 1) % map->capacity;
+    }
     return NULL;
 }
 
